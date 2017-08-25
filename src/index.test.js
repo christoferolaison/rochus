e3bb1ccfe1c3d59c910f1e6b6ed54a07a345d28a
Christofer Olaison <christofer.olaison@isotop.se>
2017-08-25 12:54:47 +0200

    use eslint-config-christofer

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,9 @@
 import splitStyleProps from '.'
 
 test('should return an empty props object and a sx object containing display key', () => {
-	expect(splitStyleProps({ display: 'flex' })).toMatchSnapshot()
+  expect(splitStyleProps({ display: 'flex' })).toMatchSnapshot()
 })
 
 test('should return an props object containing onClick and a sx object containing alignItems key', () => {
-	expect(splitStyleProps({ alignItems: 'center', onClick: () => {} })).toMatchSnapshot()
+  expect(splitStyleProps({ alignItems: 'center', onClick: () => {} })).toMatchSnapshot()
 })
-
